7529a4a650a3e9aa3048b2dd74de8c1f12aab18c
asarrafzadeh_wherete <sarrafzadeharasi.1871766@studenti.uniroma1.it>
2025-05-10 21:24:21 +0200

    05

--- scripts/05_generate_clean_csv_files.py
+++ scripts/05_generate_clean_csv_files.py
@@ -362,9 +362,6 @@ if __name__ == '__main__':
     filtered_trips_at_least_one_activitiy_inside_sim.dropna(subset=['mode'], inplace=True)
     filtered_trips_all_activities_inside_sim = filtered_trips_at_least_one_activitiy_inside_sim[
         ~filtered_trips_at_least_one_activitiy_inside_sim['mode'].isin(['truck'])]
-    filtered_trips_all_activities_inside_sim.to_csv(
-        f'{pre_processed_data_path}\\travel_time_all_activities_inside_distance_mode_sim.csv', index=False)
-    logging.info("Dataframe of simulation is saved as csv file successfully")
 
     filtered_trips_at_least_one_activitiy_inside_sim = df_trips_at_least_one_activity_inside_sim[[
         "person", "start_link", "end_link", "dep_time", "trav_time", "euclidean_distance", "longest_distance_mode",
@@ -377,6 +374,6 @@ if __name__ == '__main__':
     filtered_trips_at_least_one_activitiy_inside_sim.dropna(subset=['mode'], inplace=True)
     filtered_trips_at_least_one_activitiy_inside_sim = filtered_trips_at_least_one_activitiy_inside_sim[
         ~filtered_trips_at_least_one_activitiy_inside_sim['mode'].isin(['truck'])]
-    filtered_trips_at_least_one_activitiy_inside_sim.to_csv(
-        f'{pre_processed_data_path}\\travel_time_at_least_one_activity_inside_distance_mode_sim.csv', index=False)
-    logging.info("Dataframe of simulation is saved as csv file successfully")
+
+    filtered_trips_at_least_one_activitiy_inside_sim.to_csv(f'{data_path_clean}\\trips_at_least_one_activity_inside_sim.csv', index=False)  # no changes here, but moved to data_path_clean
+    filtered_trips_all_activities_inside_sim.to_csv(f'{data_path_clean}\\trips_all_activities_inside_sim.csv', index=False)  # no changes here, but moved to data_path_clean
