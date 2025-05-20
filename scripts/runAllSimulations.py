0cebbaef87f99233aeccceeb95a3d40ef45cd052
Rado00 <70847120+Rado00@users.noreply.github.com>
2025-05-20 11:06:38 +0200

    range allSims

--- scripts/runAllSimulations.py
+++ scripts/runAllSimulations.py
@@ -12,7 +12,7 @@ bat_file = 'allScripts.bat'  # Replace with your .bat file's name or path
 config = configparser.ConfigParser()
 # Read the existing config file
 config.read(config_path)
-simulations_list = list(range(2, 20)) #Mettere 1 in + a fine range
+simulations_list = list(range(17, 25)) #Mettere 1 in + a fine range
 base_sims_path = r'Paper2_SimsOutputs\1_ModalSplitCalibration\BaselineCalibration_NV_'
 for i in simulations_list:
     try:
